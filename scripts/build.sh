3045635a4276f6a7c102abf10388e225c8418a95
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-08-01 10:12:53 +0900

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,20 @@
+echo "\n\n\nInitializing build/"
 rm -Rf build/
+echo "\n\n\nGenerate static pages"
 static-i18n -l en -i en -i ko -o build --outputDefault __lng__/__file__ pages/
-cp src/templates/index.html build/index.html
-cp src/static/css/normalize.css build/static/css/normalize.css
-cp src/static/css/webfont.css build/static/css/webfont.css
+echo "\n----- Done"
+echo "\n\n\nCompiling SASS"
 gulp sass
 gulp autoprefix
+echo "\n----- Done"
+echo "\n\n\nMinifying js files"
 gulp js:minify
+echo "\n----- Done"
+echo "\n\n\\nOptimizing and copying assets to build/"
+gulp fonts
+gulp img:minify
+cp src/static/css/normalize.css build/static/css/normalize.css
+cp src/static/css/webfont.css build/static/css/webfont.css
+cp src/templates/index.html build/index.html
+echo "\n----- Done"
+echo "\n\n\n===Build Completed"
