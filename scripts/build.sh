44a398c2d45334f9e07c7c399b24d7fdc006a87a
jinhun shin <jinhun.shin@bespinglobal.com>
2018-08-01 16:58:13 +0900

    form link / proposal pdf file download added

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 echo "\n\n\nInitializing build/"
+rm -rf build
 echo "\n\n\nGenerate static pages"
 static-i18n -l en -i en -i ko -o build --outputDefault __lng__/__file__ pages/
 echo "\n----- Done"
@@ -12,8 +13,15 @@ echo "\n----- Done"
 echo "\n\n\\nOptimizing and copying assets to build/"
 gulp fonts
 gulp img:minify
+
+# resource copy
 cp src/static/css/normalize.css build/static/css/normalize.css
 cp src/static/css/webfont.css build/static/css/webfont.css
 cp src/templates/index.html build/index.html
+cp -rf src/static/conf build/static
+
+
+src/static/conf/PYKL_Introduction_For_Advertiser.pdf
+
 echo "\n----- Done"
 echo "\n\n\n===Build Completed"
